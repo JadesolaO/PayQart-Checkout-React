f36803475d67a993a314946aef7ecbe1c11919e0
Falore Oluwajuwon <oluwajuwonfalore@gmail.com>
2021-05-26 10:33:29 +0100

    Yes

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,8 @@
 import { toast } from 'react-toastify';
 import http from './httpService';
 
-const apiEndpoint = 'http://localhost:2000/user';
-// const apiEndpoint = 'https://cryptic-reef-51266.herokuapp.com/user';
+// const apiEndpoint = 'http://localhost:2000/user';
+const apiEndpoint = 'https://cryptic-reef-51266.herokuapp.com/user';
 
 export async function doLogin(user) {
   return await http.post(`${apiEndpoint}/login`, user);
